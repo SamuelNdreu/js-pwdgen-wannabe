3f6cbbed486b219bd3134085e857745c1bcbee2a
SamuelNdreu <samwellsoficcial@gmail.com>
2022-11-18 15:50:23 +0100

    fixed mistakes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let userName = prompt('Qual è il tuo nome?')
-let userSurname = prompt('Qual è il tuo cognome?')
-let favouriteColour = prompt('Qual è il tuo colore preferito?')
-let passWord = userName + userSurname + favouriteColour + 69
+let userName = prompt('Qual è il tuo nome?');
+let userSurname = prompt('Qual è il tuo cognome?');
+let favouriteColour = prompt('Qual è il tuo colore preferito?');
+let passWord = userName + userSurname + favouriteColour + 22
 
-document.getElementById("passwordgen").innerHTML = passWord
+document.getElementById("passwordgen").innerHTML = passWord;
